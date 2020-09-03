90e7ae44bb31d1fd922ceb160d3932dc3dfbca60
David Corner <davidcorner90@gmail.com>
2020-09-03 23:13:37 +1200

    Basic display of Fretboard (badly scaled)

--- FretsApp/FretsApp/MainWindow.xaml.cs
+++ FretsApp/FretsApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,7 @@ namespace FretsApp
     {
         public MainWindow()
         {
-            InitializeComponent();
+        InitializeComponent();
         }
     }
 }
