e1f1ac166917b37d9c763cbe9cf4ba1214991158
David Corner <davidcorner90@gmail.com>
2020-09-03 22:14:09 +1200

    Change name project

--- FretsApp/FretsApp/App.xaml.cs
+++ FretsApp/FretsApp/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Frets
+namespace FretsApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- FretsApp/FretsApp/MainWindow.xaml.cs
+++ FretsApp/FretsApp/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Frets
+namespace FretsApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
